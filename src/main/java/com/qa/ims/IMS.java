9186607c848af8b309915b3b910865d86745dbb2
tashfeenarshad <107992514+tashfeenarshad@users.noreply.github.com>
2022-07-20 12:56:24 +0100

    IMS updated to include items

--- src/main/java/com/qa/ims/IMS.java
+++ src/main/java/com/qa/ims/IMS.java
@@ -6,7 +6,9 @@ import org.apache.logging.log4j.Logger;
 import com.qa.ims.controller.Action;
 import com.qa.ims.controller.CrudController;
 import com.qa.ims.controller.CustomerController;
+import com.qa.ims.controller.ItemsController;
 import com.qa.ims.persistence.dao.CustomerDAO;
+import com.qa.ims.persistence.dao.ItemsDAO;
 import com.qa.ims.persistence.domain.Domain;
 import com.qa.ims.utils.DBUtils;
 import com.qa.ims.utils.Utils;
@@ -16,12 +18,15 @@ public class IMS {
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	private final CustomerController customers;
+	private final ItemsController items;
 	private final Utils utils;
 
 	public IMS() {
 		this.utils = new Utils();
 		final CustomerDAO custDAO = new CustomerDAO();
 		this.customers = new CustomerController(custDAO, utils);
+		final ItemsDAO itemDAO = new ItemsDAO();
+		this.items = new ItemsController(itemDAO, utils);
 	}
 
 	public void imsSystem() {
@@ -50,6 +55,7 @@ public class IMS {
 				active = this.customers;
 				break;
 			case ITEM:
+				active = this.items;
 				break;
 			case ORDER:
 				break;
